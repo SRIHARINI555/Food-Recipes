db268c9a9a239dbc5d7bb9943338e20826d419f9
SRIHARINI555 <sriharini523@gmail.com>
2023-11-21 23:29:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
+import SearchBar from './components/SearchBar';
+import RecipeCard from './components/RecipeCard';
+
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  // function to search for the recipes
+
+  const searcRecipes = async () => {
+    setIsLoading(true);
+    const url = apiUrl + query;
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log(data);
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searcRecipes();
+  }, []);
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    searcRecipes()
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="container">
+        <h2>Food Recipes App</h2>
+        <SearchBar
+             handleSubmit = {handleSubmit}
+             value = {query}
+             onChange = {event => setQuery(event.target.value)}
+             isLoading = {isLoading}
+        />
+        <div className="recipes">
+             {recipes 
+                 ? recipes.map(recipe => (
+                     <RecipeCard key={recipe.idMeal} recipe={recipe} />
+                   ))
+                : "No Recipes!"}
+        </div>
+     </div>
   );
 }
 
